3aa4772a7c31d319d30b6b2c32223767746595b1
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2017-03-25 05:02:14 +0530

    Login using firebase.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import Firebase from 'firebase';
+import firebase from 'firebase';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 // common root component, used from index.ios and index.android
 class App extends Component {
   componentWillMount() {
-    Firebase.initializeApp({
+    firebase.initializeApp({
     apiKey: 'AIzaSyB4vxedmGU__nFewiiMOSz6cY71S38Ph00',
     authDomain: 'auth-20330.firebaseapp.com',
     databaseURL: 'https://auth-20330.firebaseio.com',
     storageBucket: 'auth-20330.appspot.com',
     messagingSenderId: '1090434483492'
-  });
+    });
   }
   render() {
     return (
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Card, CardSection, Button, Input } from './common';
+
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -34,8 +52,12 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
@@ -43,5 +65,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
